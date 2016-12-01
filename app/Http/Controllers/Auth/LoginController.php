f69531d817518567b927b118df69ab30dd55cc2b
Mucyo F Miller <mucyomiller@gmail.com>
2016-12-01 16:49:59 +0200

    add Login Controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/dashboard';
+    protected $username ='staff_id';
 
     /**
      * Create a new controller instance.
