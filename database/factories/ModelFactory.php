18817a3e8362f05d1f3ae140b398ee8cdc5ef56b
Mucyo F Miller <mucyomiller@gmail.com>
2016-12-11 20:49:34 +0200

    add migrations & seeds

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,9 +15,17 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'surname' => $faker->lastName,
+        'middle_name' => $faker->name,
+        'firstname'   => $faker->firstNameMale,
+        'idcardnumber'=> $faker->creditCardNumber,
+        'passportnumber' => $faker->creditCardNumber,
+        'gender'         => "male",
+        'staff_id' 		 => $faker->randomNumber($nbDigits = 9),
+        'email' 		=> $faker->unique()->safeEmail,
+        'work_email'	=> $faker->unique()->companyEmail,
+        'registered'    => $faker->boolean($chanceOfGettingTrue = 50),
+        'password' => $password ?: $password = bcrypt('1234'),
         'remember_token' => str_random(10),
     ];
 });
