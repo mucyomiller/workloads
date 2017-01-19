0ff3504f40f7fbc99bdb039a9dceb6867c2fec34
Mucyo F Miller <mucyomiller@gmail.com>
2017-01-19 16:10:07 +0200

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,12 @@ Route::match(['get','post'],'/',['as'=>'login','uses'=>'StaffController@login'])
 Route::match(['get','post'],'/login',['as'=>'login','uses'=>'StaffController@login']);
 Route::match(['post','get'],'forget',['as'=>'forget','uses'=> 'StaffController@forget']);
 Route::match(['post','get'],'register',['as'=>'register','uses'=>'StaffController@register']);
+Route::post('setpassword',['as'=>'setpassword','uses'=>'StaffController@setpassword']);
 //dashboard routes
 Route::group(['prefix'=>'dashboard'],function(){
 	//Route::get('/', ['uses'=>'StaffController@index', 'as'=>'dashboard.index']);
 	Route::get('index',['as'=>'dashboard.index','uses'=>'StaffController@home']);
+	Route::get('profile',['as'=>'dashboard.profile','uses'=>'StaffController@profile']);
   	//Route::get('delete', ['uses'=>'StaffController@delete', 'as'=>'delete']);
   	//Route::match(['get', 'post'],'courses', ['uses'=>'CourseController@index', 'as'=>'courses']);
   	Route::get('logout',['as'=>'dashboard.logout','uses'=> function(){
