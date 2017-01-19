7ec9aa69c7c42bb3a55980eacf9079a66cc782bb
Bi Theo <Bi Theo>
2017-01-19 20:25:18 +0200

    hel

--- resources/views/dashboard/theme.blade.php
+++
@@ -1,5 +0,0 @@
-@include('dashboard.header')
-<!-- Page Content Start -->
-@yield('content')
-<!-- Page Content Ends -->
-@include('dashboard.footer')
--- resources/views/frontend/theme.blade.php
+++
@@ -1,5 +0,0 @@
-@include('frontend.header')
-<!-- Page Content Start -->
-@yield('content')
-<!-- Page Content Ends -->
-@include('frontend.footer')
